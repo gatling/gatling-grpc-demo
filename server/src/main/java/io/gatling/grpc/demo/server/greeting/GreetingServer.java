e601688a1434e446cfabb81802dbfc3c2017f6e2
Guillaume Galy <ggaly@gatling.io>
2024-03-04 11:39:26 +0100

    feat: Print client certificate in GreetingServer

--- server/src/main/java/io/gatling/grpc/demo/server/greeting/GreetingServer.java
+++ server/src/main/java/io/gatling/grpc/demo/server/greeting/GreetingServer.java
@@ -1,6 +1,8 @@
 package io.gatling.grpc.demo.server.greeting;
 
 import java.io.IOException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLPeerUnverifiedException;
 
 import io.gatling.grpc.demo.server.Configuration;
 
@@ -10,8 +12,8 @@ public class GreetingServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         ServerCredentials credentials = Configuration.credentials();
         Server server = Grpc.newServerBuilderForPort(50051, credentials)
-            .addService(new GreetingServiceImpl())
-            .build();
+                .addService(ServerInterceptors.intercept(new GreetingServiceImpl(), new MyInterceptor()))
+                .build();
         server.start();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Received shutdown request");
@@ -19,5 +21,20 @@ public class GreetingServer {
             System.out.println("Successfully stopped the server");
         }));
         server.awaitTermination();
+    }
+
+    public static class MyInterceptor implements ServerInterceptor {
+        @Override
+        public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+                ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+            try {
+                var sslSession = call.getAttributes().get(Grpc.TRANSPORT_ATTR_SSL_SESSION);
+                var certificate = (X509Certificate) sslSession.getPeerCertificates()[0];
+                System.out.println("Received greeting from: " + certificate.getSubjectX500Principal());
+            } catch (SSLPeerUnverifiedException e) {
+                e.printStackTrace();
+            }
+            return next.startCall(call, headers);
+        }
     }
 }
