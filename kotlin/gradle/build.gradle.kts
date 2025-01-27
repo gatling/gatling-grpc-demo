248d1389cd411daf8cf553dcc78c87f2d607c36c
Guillaume Corré <gui.corre@gmail.com>
2025-01-27 10:18:06 +0100

    chore: update gatling-grpc 3.13.3.1

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
   gatlingApi("com.google.protobuf:protobuf-kotlin:3.25.6")
-  gatlingImplementation("io.gatling:gatling-grpc-java:3.13.3")
+  gatlingImplementation("io.gatling:gatling-grpc-java:3.13.3.1")
 }
 
 gatling {
