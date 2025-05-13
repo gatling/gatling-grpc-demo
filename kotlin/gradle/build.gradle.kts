2179844b103181cedc88f2f5e396fa8fa4272b36
slandelle <slandelle@gatling.io>
2025-05-13 07:44:50 +0200

    feat: upgrade Gatling 3.14.1

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
   id("com.google.protobuf")   version "0.9.5"
   id("com.diffplug.spotless") version "7.0.3"
-  id("io.gatling.gradle")     version "3.14.0.2"
+  id("io.gatling.gradle")     version "3.14.1"
 }
 
 repositories {
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
   gatlingApi("com.google.protobuf:protobuf-kotlin:3.25.7")
-  gatlingImplementation("io.gatling:gatling-grpc-java:3.14.0")
+  gatlingImplementation("io.gatling:gatling-grpc-java:3.14.1")
 }
 
 gatling {
