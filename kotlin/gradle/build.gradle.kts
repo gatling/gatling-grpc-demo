54728f6fc817894ea776ee2819931a994e844230
slandelle <slandelle@gatling.io>
2025-05-15 17:02:51 +0200

    chore: upgrade io.gatling.gradle 3.14.1.3

--- kotlin/gradle/build.gradle.kts
+++ kotlin/gradle/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
   id("com.google.protobuf")   version "0.9.5"
   id("com.diffplug.spotless") version "7.0.3"
-  id("io.gatling.gradle")     version "3.14.1"
+  id("io.gatling.gradle")     version "3.14.1.3"
 }
 
 repositories {
